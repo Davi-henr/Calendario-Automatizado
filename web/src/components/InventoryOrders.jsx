2ed9c817b16f6495a95dcd5be6d00b5bd9138ec1
Davi Henrique <davihennriquebuenno@gmail.com>
2026-04-09 10:51:47 -0300

    Update email recipients in InventoryOrders component

--- web/src/components/InventoryOrders.jsx
+++ web/src/components/InventoryOrders.jsx
@@ -159,7 +159,7 @@ export default function InventoryOrders({ subview = 'fazer', onNavigate }) {
                     data_entrega_completa: dataEntregaCompleta,
                     data_entrega_curta: dataEntregaCurta,
                     tabela_produtos: tabelaHTML,
-                    email: "davi.fvl@markbemcitrus.com.br"
+                    email: "vania.fvl@hotmail.com, fvl.financeiro@markbemcitrus.com.br, f.alegria@markbemcitrus.com.br"
                 };
 
                 // 3. Enviar o E-mail
